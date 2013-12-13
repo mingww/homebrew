f42b4c60450e20a10b523979c0b3916e4f5ce59f
Ming Ma <ming@weddingwire.com>
2013-12-13 13:40:12 -0500

    new zip

--- Library/Formula/phantomjs.rb
+++ Library/Formula/phantomjs.rb
@@ -4,8 +4,8 @@ class Phantomjs < Formula
   #homepage 'http://www.phantomjs.org/'
   #url 'https://phantomjs.googlecode.com/files/phantomjs-1.9.2-source.zip'
   #sha1 '08559acdbbe04e963632bc35e94c1a9a082b6da1'
-  url 'https://www.dropbox.com/s/kro7d5nt9btpl01/phantomjs-1.9.2-PATCH-source.zip'
-  sha1 'f2a88dc98bae19c486d737cffd477ef51a597897'
+  url 'https://www.dropbox.com/s/mrwz0j60yvh3u3j/phantomjs-1.9.2.zip'
+  sha1 'af5d467a27996e3a76afe58901c5abac260fbfb9'
 
   def patches
     DATA
